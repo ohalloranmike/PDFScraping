662daae54c491147872ad59d99d4724b5155ef58
[Mike] <[ohalloranmike@outlook.com]>
2024-11-21 18:08:21 -0800

    Added input for what_to_do.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pdfplumber
 from tkinter.filedialog import askopenfilename
 
 
-# Extracts all text in a pdf file and displays returns it with page numbers.
+# Extracts all text in a pdf file.
 def extract_pdf_text(pdf_path):
     with pdfplumber.open(pdf_path) as book:
         text_all = ''
@@ -10,8 +10,7 @@ def extract_pdf_text(pdf_path):
             text_all += page.extract_text()
     return text_all
 
-
-# Extracts all text in a pdf file and displays returns it with page numbers.
+# Extracts all text in a pdf file and returns it with page numbers.
 def extract_pdf_text_w_p(pdf_path):
     with pdfplumber.open(pdf_path) as book:
         text_all = ''
@@ -21,7 +20,6 @@ def extract_pdf_text_w_p(pdf_path):
             c = c + 1
     return text_all
 
-
 # Extracts the text for a specific page, and returns an error if page is out of bounds.
 def extract_page_text(pdf_path, page_number):
     with pdfplumber.open(pdf_path) as book:
@@ -30,8 +28,20 @@ def extract_page_text(pdf_path, page_number):
         else:
             raise ValueError(f"Page number {page_number} is out of range.")
 
+# Extracts all text (line by line) in a pdf file.
+def extract_pdf_lines(pdf_path):
+    with pdfplumber.open(pdf_path) as book:
+        lines = []
+        for each_page in book.pages:
+            page_lines = each_page.extract_text_lines()
+            for each_line in page_lines:
+                text = each_line['text']
+                lines.append({
+                    'text': text,
+                })
+    return lines
 
-# Extracts all text (line by line) in a pdf file and returns with page & line numbers.
+# Extracts all text (line by line) in a pdf file and returns them with page & line numbers.
 def extract_pdf_lines_w_pl(pdf_path):
     with pdfplumber.open(pdf_path) as book:
         lines = []
@@ -47,20 +57,6 @@ def extract_pdf_lines_w_pl(pdf_path):
                 line = line + 1
             page = page + 1
             line = 0
-    return lines
-
-
-# Extracts all text (line by line) in a pdf file.
-def extract_pdf_lines(pdf_path):
-    with pdfplumber.open(pdf_path) as book:
-        lines = []
-        for each_page in book.pages:
-            page_lines = each_page.extract_text_lines()
-            for each_line in page_lines:
-                text = each_line['text']
-                lines.append({
-                    'text': text,
-                })
     return lines
 
 
